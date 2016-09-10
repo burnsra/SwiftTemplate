7fd7f9b1fdb1339874821e0a54aa40ca3b8ce793
Robert Burns <burnsra@gmail.com>
2016-09-09 23:04:35 -0400

    Initial Commit

---
+++ SwiftTemplate/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  SwiftTemplate
+//
+//  Created by Robert Burns on 9/9/16.
+//  Copyright © 2016 Robert Burns. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
